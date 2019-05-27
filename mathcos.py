b02ea63a5ece6eeea31ba7738e7b4510452232c7
easycheneasy <easycheneasy@gmail.com>
2019-05-27 11:13:09 +0800

    practice done

--- mathcos.py
+++ mathcos.py
@@ -1,8 +1,17 @@
 # use math to get cosine value
 
 import math
+
 print(math.cos(0.5))
 print("Atan(2)=", math.atan(2))
 print("sin(1)=", math.sin(1))
+area_of_circle_v2 = math.pi * 10 * 10
+print("area of circle is ", area_of_circle_v2)
+# print(dir(math))
+print(f"area of circle is second format {area_of_circle_v2}")
+if True:
+    print("True is true")
+elif False:
+    print("GOGO")
 
-#print(dir(math))
+print(f"")
